e97c8a30373766ebf6d6f348974d0876ecfb9c3b
HISAYO <hisayohorie@gmail.com>
2016-04-13 20:02:49 -0400

    adding logic to display 'signed in as' or 'login or sign up in the application view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to products_url,  notice => "Logged in!" #<= why does this go to products_url?
+      redirect_to products_url,  notice: "Logged in!" #<= why does this go to products_url?
     else
       flash.now[:alert] = "Invalid email or password"
       render "new"
